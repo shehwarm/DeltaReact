c3e0d699e83214b6c12226d2050e5e76bb51fc09
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-22 19:55:21 +0500

    first commit

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+import Title from "./Title.jsx";
+
+function Description(){
+  return <h1>I am the Description</h1>;
+}
+
+function App() {
+  return (
+     <div>
+         <h1>this is my app</h1>
+        <Title />
+        <Description />
+     </div>
+  )
+}
+
+export default App
