9d1f8a009c2d66c622abe0ceddb8dfbfd8de6845
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-25 15:29:21 +0500

    fix: apply dynamic background color to Product component based on price

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,9 +1,9 @@
 import "./Product.css";
 
 function Product({title, price, features}) {
-
+ let styles = {backgroundColor: price > 3000 ? "yellow" : ""};
     return(
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <p>Product description</p>
             <h5>price : {price}</h5>
