8500f0e578fce3f597ca75acb0f6f48d3be80868
Shehwar Maryam <shehwarm93@gmail.com>
2025-10-27 01:18:59 +0500

    feat: enhance Button component with additional event handlers and interactions

--- basic-react-app/src/Button.jsx
+++ basic-react-app/src/Button.jsx
@@ -1,11 +1,18 @@
 function printHello(){
     console.log("Hello from Button component");
 }
-
+function printBye(){
+    console.log("Bye from Button component");
+}
+function handleDbClicked(){
+    console.log("Double Clicked");
+}
 export default function Button(){
     return (
         <div>
         <button onClick={printHello}>Click me</button>
+        <p onMouseOver={printBye}>nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
+        <button onDoubleClick={handleDbClicked}>double click me</button>
         </div>
     );
-}
+} 
