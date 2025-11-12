78861d24e60d9318808f2c95a80e144d94dc89b6
Shehwar Maryam <shehwarm93@gmail.com>
2025-11-12 17:03:57 +0500

    fix: update Counter component to initialize count with random value and simplify increment logic

--- react-states-class/src/Counter.jsx
+++ react-states-class/src/Counter.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
+function init() {
+  console.log("Init function called");
+  return Math.random();
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(init());
   console.log("Counter rendered");
 
   const incCount = () => {
-    setCount((currCount) => {
-      return currCount + 1;
-    });
-    setCount((currCount) => {
-      return currCount + 1;
-    });
+   // setCount((currCount) => {
+   //   return currCount + 1;
+   // });
+   // setCount((currCount) => {
+   //   return currCount + 1;
+    //});
+    setCount(1);
   };
 
   return (
