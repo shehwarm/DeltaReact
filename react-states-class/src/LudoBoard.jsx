508894130d24a41564c898ed7c6130ae4d328ff5
Shehwar Maryam <shehwarm93@gmail.com>
2025-11-12 23:18:33 +0500

    fix: correct update logic for moves in LudoBoard component and add missing onClick handlers

--- react-states-class/src/LudoBoard.jsx
+++ react-states-class/src/LudoBoard.jsx
@@ -4,9 +4,24 @@ export default function LudoBoard(){
     let[moves, setMoves] = useState({blue : 0, red: 0, green: 0, yellow: 0});
 
     let updateBlue = () => {
-        moves.blue += 1;
-        console.log(`moves = ${moves.blue}`);
-        setMoves({...moves, blue: moves.blue +1});
+        setMoves((prevMoves) => {
+            return {...prevMoves, yellow: prevMoves.yellow + 1};
+        });
+    };
+     let updateYellow = () => {
+        setMoves((prevMoves) => {
+            return {...prevMoves, yellow: prevMoves.yellow + 1};
+        });
+    };
+    let updateGreen = () => {
+        setMoves((prevMoves) => {
+            return {...prevMoves, green: prevMoves.green + 1};
+        });
+    };
+    let updateRed = () => {
+        setMoves((prevMoves) => {
+            return {...prevMoves, red: prevMoves.red + 1};
+        });
     };
 
    return (
@@ -16,11 +31,11 @@ export default function LudoBoard(){
             <p>Blue moves = {moves.blue}</p>
             <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1 </button>
             <p>Yellow moves = {moves.yellow}</p>
-            <button style={{backgroundColor:"yellow", color:"black"}}>+1 </button>
+            <button style={{backgroundColor:"yellow", color:"black"}} onClick={updateYellow}>+1 </button>
             <p>Green moves = {moves.green}</p>
-            <button style={{backgroundColor:"green"}}>+1 </button>
+            <button style={{backgroundColor:"green"}} onClick={updateGreen}>+1 </button>
             <p>Red moves = {moves.red}</p>
-            <button style={{backgroundColor:"red"}}>+1 </button>
+            <button style={{backgroundColor:"red"}} onClick={updateRed}>+1 </button>
         </div>
     </div>
    ) 
