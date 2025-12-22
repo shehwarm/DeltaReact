30170c56bf2b2de7d4b2321abbc62be76ea3730a
Shehwar Maryam <shehwarm93@gmail.com>
2025-12-22 20:06:02 +0500

    feat: pass winCondition function to Lottery component for winning logic

--- react-states-class/src/App.jsx
+++ react-states-class/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Lottery from './Lottery';
+import { sum } from './helper';
 
 function App() {
+
+   let winConditon = (ticket) => {
+     return sum(ticket) === 15;
+   }
+
   return <>
    
-    <Lottery n={3} winningSum={15} />
+    <Lottery n={3} winningSum={15} winConditon={winConditon} />
 
   </>;
  
