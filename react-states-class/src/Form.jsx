043c4e2cb3af5e6b222d181eff88242babba46ad
Shehwar Maryam <shehwarm93@gmail.com>
2025-12-23 19:21:45 +0500

    fix: add label for username input in Form component for accessibility

--- react-states-class/src/Form.jsx
+++ react-states-class/src/Form.jsx
@@ -9,7 +9,14 @@ export default function Form() {
 
     return (
         <form>
-            <input placeholder="enter full name"type="text" value={fullName} onChange={handleNameChange}/>
+            <label htmlFor="username">Full Name</label>
+            <input 
+             placeholder="enter full name"
+             type="text" 
+             value={fullName}
+             onChange={handleNameChange}
+             id="username"
+             />
             <button>Submit</button>
         </form>
     );
